044267c859ccd22e4e1402bd8728253a7e55cb8f
kazadi <kazadix2@gmail.com>
2018-05-13 17:59:23 -0500

    added delete to showPost

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'fetch_posts';
-export const CREATE_POST = 'creat_post';
+export const FETCH_SINGLE_POST = 'fetch_sing_post';
+export const CREATE_POST = 'create_post';
+export const DELETE_POST = 'delete_post';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=mattress224';
@@ -24,4 +26,22 @@ export function createPost(values, callback) {
         payload: request
     }
 
+}
+
+export function fetchSinglePost(id) {
+    const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`);
+
+    return {
+        type: FETCH_SINGLE_POST,
+        payload: request
+    };
+}
+
+export function deletePost(id, callback) {
+    const request = axios.delete(`${ROOT_URL}/posts/${id}${API_KEY}`)
+        .then(() => callback());
+    return {
+        type: DELETE_POST,
+        payload: id
+    }
 }
